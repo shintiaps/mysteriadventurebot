6931a4195e01c81008925f97604949de3b7631e1
shintia <shintiaputrisalamah20@gmail.com>
2026-02-09 04:55:56 +0000

    mysteri adventure bot v1.0

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+
+def game_utama():
+    print("--- MEMULAI PETUALANGAN DIGITAL ---")
+    nama = input("Siapa namamu? ")
+    # Silahkan lanjutkan alur ceritamu di bawah ini menggunakan AI
+    
+if __name__ == "__main__":
+    game_utama()
